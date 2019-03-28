52c33d38ffdfb4aa1e33af7a259ee7defd7da6f3
RemyMaige <46453539+RemyMaige@users.noreply.github.com>
2019-03-29 00:57:04 +0100

    first commit

---
+++ Assets/knightBehavior.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    private Animator m_Animator;
+    private bool charge = false;
+    private bool demo = false;
+    private bool idle = true;
+
+    public Vuforia.ImageTarget swordMarker;
+    public Vuforia.Image shieldMarker;
+    public Vuforia.CameraDevice cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (swordMarker.)
+        {
+            demo = false;
+            m_Animator.SetBool("lk_heavy_infantry_06_combat_walk", demo);
+            charge = !charge;
+            m_Animator.SetBool("WK_heavy_infantry_04_charge 0", charge);
+        }
+
+        if (shieldMarker.)
+        {
+            charge = false;
+            m_Animator.SetBool("WK_heavy_infantry_04_charge 0", charge);
+            demo = !demo;
+            m_Animator.SetBool("lk_heavy_infantry_06_combat_walk", demo);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            demo = false;
+            charge = false;
+            m_Animator.SetBool("lk_heavy_infantry_06_combat_walk", demo);
+            m_Animator.SetBool("WK_heavy_infantry_04_charge 0", charge);
+        }
+
+    }
+}
